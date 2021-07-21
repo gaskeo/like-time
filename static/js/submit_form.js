bcd6561c378250fd9ef315a16899c892e3ccc1c0
Tikovka72 <zeytunyan2004@mail.ru>
2021-07-21 20:04:24 +0500

    js refactor and small css improves

--- static/js/submit_form.js
+++ static/js/submit_form.js
@@ -2,16 +2,18 @@ function showResult() {
     let link_or_username = document.getElementById("account-link-input")
 
     if (!(link_or_username.value.replace(/\s/g, '').length)) {
-        link_or_username.placeholder = "впиши в меня что-нибудь пожалуйста..."
+        link_or_username.placeholder = "впиши в меня что-нибудь, пожалуйста..."
         link_or_username.value = ""
         return
     }
+
     let form = new FormData()
     form.append("shortcode_or_link", link_or_username.value)
-    document.getElementById("error").style.display = "none"
 
+    document.getElementById("error").style.display = "none"
     document.getElementById("loading").style.display = "block"
     document.getElementById("send-button").disabled = true
+
     if (document.getElementById("table-likes")) {
         document.getElementById("table-likes").remove()
     }
@@ -23,69 +25,86 @@ function showResult() {
                     if (j["answer"] !== "ok") {
                         document.getElementById("error").style.display = "block"
                         document.getElementById("error").textContent = j["error-message"]
-                        alert(j["answer"])
+                        document.getElementById("loading").style.display = "none"
+                        document.getElementById("send-button").disabled = false
                     } else {
                         let width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
                         document.getElementById("loading").style.display = "none"
+
                         let table = document.createElement("table")
                         table.id = "table-likes"
+
                         let row = document.createElement("tr")
                         row.appendChild(document.createElement("th"))
+
                         for (let i = 1; i < j["posts"].length + 1; i++) {
                             let post_cell = document.createElement("th")
+
                             let post_link = document.createElement("a")
                             post_link.href = "https://instagram.com/p/" + j["posts"][i - 1]
+
                             let plus
                             if (width > 999) {
                                 plus = "пост "
                             } else plus = ""
+
                             let link_text = document.createTextNode(plus + i.toString())
 
-                            post_link.title = i.toString()
                             post_link.appendChild(link_text)
                             post_cell.appendChild(post_link)
                             row.appendChild(post_cell)
                         }
+
                         table.appendChild(row)
+
                         let refactored_data = mapToArray(j["users"])
                         refactored_data.sort(function (a, b) {
                             return b[1].length - a[1].length || a[0].localeCompare(b[0])
                         })
+
                         for (let i = 0; i < refactored_data.length; i++) {
                             let row = document.createElement("tr")
+
                             let user = document.createElement("th")
+
                             let user_link = document.createElement("a")
                             user_link.href = "https://instagram.com/" + refactored_data[i][0]
+
                             let link_text = document.createTextNode(refactored_data[i][0])
+
                             user_link.title = refactored_data[i][0]
                             user_link.appendChild(link_text)
+
                             user.appendChild(user_link)
                             user.className = "user-th"
+
                             row.appendChild(user)
+
                             let likes = refactored_data[i][1]
+
                             for (let post = 0; post < 10; post++) {
                                 let like = document.createElement("th")
+
                                 if (likes.includes(post)) {
                                     like.textContent = '\u2764'
                                     like.className = "heart"
                                 }
                                 row.appendChild(like)
-
                             }
                             table.appendChild(row)
                         }
-                        loading.after(table)
+                        document.getElementById("loading").after(table)
                         document.getElementById("send-button").disabled = false
-
                     }
                 })
             } else {
                 document.getElementById("error").style.display = "block"
                 document.getElementById("error").textContent = "что-то пошло не так, " +
                     "я даже не знаю, что именно..."
-                    document.getElementById("loading").style.display = "none"
-                document.getElementById("send-button").disabled = false
 
+                document.getElementById("loading").style.display = "none"
+                document.getElementById("send-button").disabled = false
             }
         }
     )
