c6dcda5a24541281598da25844f161dd8a1ac5ad
Tikovka72 <zeytunyan2004@mail.ru>
2021-07-24 11:07:48 +0500

    small refactor in inst api

--- api/inst_api.py
+++ api/inst_api.py
@@ -4,6 +4,10 @@ from json import dumps
 from json.decoder import JSONDecodeError
 
 
+class EmptyCookieError(Exception):
+    ...
+
+
 class InstApi:
     headers = {
         'user-agent':
@@ -14,11 +18,14 @@ class InstApi:
 
     def __init__(self, cookies):
         self.session = session()
-        [self.session.cookies.set(n, m) for n, m in cookies.items()]
+        if cookies:
+            [self.session.cookies.set(n, m) for n, m in cookies.items()]
+        else:
+            raise EmptyCookieError()
+
         self.query_hash_for_likes = "d5d763b1e2acf209d62d22d184488e57"
         self.query_hash_for_posts = "ea4baf885b60cbf664b34ee760397549"
         self.query_hash_for_post = "2efa04f61586458cef44441f474eee7c"
-        self.query_hash_for_account_id = "dbdfd83895d23a4a0b0f68a85486e91c"
 
     def get_user_liked_post(self, post_shortcode):
         variables = {
@@ -26,10 +33,12 @@ class InstApi:
             "include_reel": False,
             "first": 50
         }
+
         g = self.session.get("https://www.instagram.com/graphql/query/", params={
             "query_hash": self.query_hash_for_likes,
             "variables": dumps(variables)
         })
+
         try:
             json = g.json()
             if not json["data"]["shortcode_media"]:
@@ -38,6 +47,7 @@ class InstApi:
             return None
         except KeyError:
             return None
+
         count_liked = json["data"]["shortcode_media"]["edge_liked_by"]["count"]
         count_parsed = 50
         user_liked = [x["node"]["username"] for x in g.json()["data"]["shortcode_media"]["edge_liked_by"]["edges"]]
@@ -51,6 +61,7 @@ class InstApi:
                 continue
             except KeyError:
                 return None
+
             after = json["data"]["shortcode_media"]["edge_liked_by"]["page_info"]["end_cursor"]
 
             variables = {
@@ -65,26 +76,33 @@ class InstApi:
                 "variables": dumps(variables)
             })
 
-            user_liked += [x["node"]["username"] for x in g.json()["data"]["shortcode_media"]["edge_liked_by"]["edges"]]
+            user_liked += [x["node"]["username"]
+                           for x in g.json()["data"]["shortcode_media"]["edge_liked_by"]["edges"]]
             count_parsed += 50
+
         return user_liked
 
     def get_user_id_by_post_shortcode(self, shortcode):
+        variables = {"shortcode": shortcode,
+                     "child_comment_count": "0",
+                     "fetch_comment_count": "0",
+                     "parent_comment_count": "0",
+                     "has_threaded_comments": True
+                     }
+
         response = self.session.get("https://www.instagram.com/graphql/query/", params={
             "query_hash": self.query_hash_for_post,
-            "variables": dumps({"shortcode": shortcode,
-                                "child_comment_count": "0",
-                                "fetch_comment_count": "0",
-                                "parent_comment_count": "0",
-                                "has_threaded_comments": True
-                                })
+            "variables": dumps(variables)
         })
+
         try:
             json = response.json()
         except JSONDecodeError:
             return None
+
         if json["data"]["shortcode_media"]:
             return json["data"]["shortcode_media"]["owner"]["id"]
+
         return None
 
     def get_10_posts_by_user_id(self, user_id):
@@ -92,14 +110,17 @@ class InstApi:
             "id": user_id,
             "first": 10
         }
+
         response = self.session.get("https://www.instagram.com/graphql/query", params={
             "query_hash": self.query_hash_for_posts,
             "variables": dumps(variables)
         })
+
         try:
             posts = response.json()
         except JSONDecodeError:
             return None
+
         return [x["node"]["shortcode"] for x in posts["data"]["user"]["edge_owner_to_timeline_media"]["edges"]]
 
     def get_user_id_by_username(self, username):
@@ -109,12 +130,14 @@ class InstApi:
             json = response.json()
         except JSONDecodeError:
             return None
+
         if json:
             for user in json["users"]:
                 if user["user"]["username"] == username:
                     if user["user"]["is_private"]:
                         return None
                     return user["user"]["pk"]
+
         return None
 
     def check_account_exist(self, username):
