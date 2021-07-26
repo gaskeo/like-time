7903477628373fb2de7af19f7acc4b1c54108440
Tikovka72 <zeytunyan2004@mail.ru>
2021-07-26 16:52:10 +0500

    refactor typos

--- blueprints/do_search.py
+++ blueprints/do_search.py
@@ -15,7 +15,7 @@ def create_handler(api: InstApi, redis_api: Redis):
     @app.route("/do_search", methods=["POST"])
     def do_search():
         if redis_api.get(request.remote_addr):
-            return jsonify({"answer": "error", "error-message": "много запросов шлешь, меня в инсте заблокируюст... "
+            return jsonify({"answer": "error", "error-message": "много запросов шлешь, меня в инсте заблокируют... "
                                                                 "5 секунд между запросами должно быть..."})
         form = SearchForm()
         username_or_link = form.username_or_link.data
@@ -31,7 +31,7 @@ def create_handler(api: InstApi, redis_api: Redis):
             if not user_id:
                 logger.error(f"{request.remote_addr} invalid username caught: {user_id}")
                 return jsonify({"answer": "error", "error-message": "пользователь не найден,"
-                                                                    " возмжоно аккаунт приватный"})
+                                                                    " возможно аккаунт приватный"})
         else:
             parse_link = urlparse(username_or_link)
             path = parse_link.path
@@ -41,19 +41,19 @@ def create_handler(api: InstApi, redis_api: Redis):
                 user_id = api.get_user_id_by_post_shortcode(sections[-1])
                 if not user_id:
                     logger.error(f"{request.remote_addr} invalid username caught: {user_id}")
-                    return jsonify({"answer": "error", "error-message": "пост не найден, возмжоно аккаунт приватный"})
+                    return jsonify({"answer": "error", "error-message": "пост не найден, возможно аккаунт приватный"})
             else:
                 user_id = api.get_user_id_by_username(sections[-1])
                 if not user_id:
                     logger.error(f"{request.remote_addr} invalid username caught: {user_id}")
                     return jsonify({"answer": "error", "error-message": "пользователь не найден,"
-                                                                        " возмжоно аккаунт приватный"})
+                                                                        " возможно аккаунт приватный"})
 
         all_users = dict()
         posts = api.get_10_posts_by_user_id(user_id)
         if not posts:
             logger.error(f"{request.remote_addr} invalid posts caught: {user_id}")
-            return jsonify({"answer": "error", "error-message": "посты не найдены, возмжоно аккаунт приватный"})
+            return jsonify({"answer": "error", "error-message": "посты не найдены, возможно аккаунт приватный"})
         post_links = []
         for i, post in enumerate(posts):
             post_links.append(post)
